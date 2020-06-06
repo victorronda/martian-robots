4b877d97804efea36d4b22c703bddb1432f7d60d
Víctor Ronda <victor.ronda@gmail.com>
2020-06-06 10:22:58 +0200

    my first commit

---
+++ src/service/input_service.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt','utf8').split("\n").filter(elem=>elem);
+
+const [gridData, ...rawRobotsData] = input;
+
+let robotsData = rawRobotsData.map((data, index) => index % 2 === 0 && data + ' ' + rawRobotsData[index + 1]).filter(elem=>elem);
+
+module.exports = {
+    gridData,
+    robotsData
+}
